47e88416cffd3bc7b63c50eef319cc9303b16d29
Justin Dalton <jdalton15@gmail.com>
2018-12-07 09:16:23 -0500

    test to write additional text on paper containing existing text

--- src/__tests__/pencil.test.js
+++ src/__tests__/pencil.test.js
@@ -5,5 +5,13 @@ test('the pencil can write on a blank paper', () => {
   const pencil = createPencil();
   const text = 'Here is some new text';
   const result = pencil.write(paper, text);
-  expect(result).toEqual(text);
+  expect(result).toEqual('Here is some new text');
+});
+
+test('the pencil can write additional text on paper with existing text', () => {
+  const paper = 'This paper has some text on it';
+  const pencil = createPencil();
+  const text = 'and I want to add some more';
+  const result = pencil.write(paper, text);
+  expect(result).toEqual('This paper has some text on it and I want to add some more');
 });
