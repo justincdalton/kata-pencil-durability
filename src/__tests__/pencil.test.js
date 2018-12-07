727a2f4ba30586fa3e4b70a0c853d5716e78a6a6
Justin Dalton <jdalton15@gmail.com>
2018-12-07 10:12:15 -0500

    test that pencil with add blank spaces when it runs out of durability

--- src/__tests__/pencil.test.js
+++ src/__tests__/pencil.test.js
@@ -1,6 +1,6 @@
 const createPencil = require('../create-pencil');
 
-test('the pencil can write on a blank paper', () => {
+test('the pencil writes on a blank paper', () => {
   const paper = '';
   const pencil = createPencil();
   const text = 'Here is some new text';
@@ -8,10 +8,20 @@ test('the pencil can write on a blank paper', () => {
   expect(result).toEqual('Here is some new text');
 });
 
-test('the pencil can write additional text on paper with existing text', () => {
+test('the pencil writes additional text on paper with existing text', () => {
   const paper = 'This paper has some text on it';
   const pencil = createPencil();
   const text = 'and I want to add some more';
   const result = pencil.write(paper, text);
   expect(result).toEqual('This paper has some text on it and I want to add some more');
+});
+
+test('the pencil writes blank spaces when it runs out of durability', () => {
+  const paper = '';
+  const pencil = createPencil({
+    durability: 10,
+  });
+  const text = 'How much will this write?';
+  const result = pencil.write(paper, text);
+  expect(result).toEqual('How much wi              ');
 });
--- src/create-pencil.js
+++ src/create-pencil.js
@@ -1,7 +1,31 @@
-const createPencil = () => ({
-  write(paper, text) {
-    return `${paper}${paper && ' '}${text}`;
-  },
-});
+const createPencil = (input = {}) => {
+  let { durability } = input;
+
+  const reduceDurability = (amount) => {
+    durability -= amount;
+  };
+
+  return {
+    write(paper, text) {
+      const processedText = text.split('').reduce((prev, char) => {
+        if (durability < 1 || char === ' ') return `${prev} `;
+
+        if (char === char.toLowerCase()) {
+          reduceDurability(1);
+          return `${prev}${char}`;
+        }
+
+        if (char === char.toUpperCase()) {
+          reduceDurability(2);
+          return `${prev}${char}`;
+        }
+
+        return prev;
+      }, '');
+
+      return `${paper}${paper && ' '}${processedText}`;
+    },
+  };
+};
 
 module.exports = createPencil;
