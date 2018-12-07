e38b061cf8641ee643d68114d1fc5c4316e91376
Justin Dalton <jdalton15@gmail.com>
2018-12-07 10:14:13 -0500

    refactoring text processing as a separate function

--- src/create-pencil.js
+++ src/create-pencil.js
@@ -5,23 +5,25 @@ const createPencil = (input = {}) => {
     durability -= amount;
   };
 
+  const processText = text => text.split('').reduce((prev, char) => {
+    if (durability < 1 || char === ' ') return `${prev} `;
+
+    if (char === char.toLowerCase()) {
+      reduceDurability(1);
+      return `${prev}${char}`;
+    }
+
+    if (char === char.toUpperCase()) {
+      reduceDurability(2);
+      return `${prev}${char}`;
+    }
+
+    return prev;
+  }, '');
+
   return {
     write(paper, text) {
-      const processedText = text.split('').reduce((prev, char) => {
-        if (durability < 1 || char === ' ') return `${prev} `;
-
-        if (char === char.toLowerCase()) {
-          reduceDurability(1);
-          return `${prev}${char}`;
-        }
-
-        if (char === char.toUpperCase()) {
-          reduceDurability(2);
-          return `${prev}${char}`;
-        }
-
-        return prev;
-      }, '');
+      const processedText = processText(text);
 
       return `${paper}${paper && ' '}${processedText}`;
     },
